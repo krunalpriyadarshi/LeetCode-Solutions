b1148ace634102641d19a54aa9945319af85bdc5
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2023-11-29 08:39:01 -0500

    improvement to loop II

--- Solved/Java Loops II/Solution.java
+++ Solved/Java Loops II/Solution.java
@@ -1,25 +1,32 @@
 import java.util.*;
-import java.io.*;
 
-class Solution{
+class Solution {
     public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
-            
-            int sum= b;
-            int mul= 1;
-            System.out.print(a+sum+" ");
-            for(int x=2; x<=n; x++){
-                mul*=2;
-                sum+=(mul*b);
-                System.out.print(a+sum+" ");
-            }   
-            System.out.println("");         
+        try{
+            int t=in.nextInt();
+            for(int i=0;i<t;i++){
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int n = in.nextInt();
+                
+                StringBuilder result= new StringBuilder();
+                int sum = a + b;
+                int mul = 1;
+                result.append(sum).append(' ');
+                for(int x = 2; x <= n; x++){
+                    mul *= 2;
+                    sum += (mul * b);
+                    result.append(sum).append(' ');
+                }   
+                System.out.println(result.toString().trim());         
+            }
         }
-        in.close();
+        catch(InputMismatchException e){
+            System.out.println("Invalid input. Please enter integers.");
+        }
+        finally{
+            in.close();
+        }
     }
 }
---
+++ Tricks.md
@@ -0,0 +1,7 @@
+In loop, "StringBuilder" is better to use inplace of String Concatenation:
+        Strings are immutale while StringBuilder are mutable.
+        ex.,
+            String Concatenation:
+                String str= a+ b+ c; //a,b,c are string variable.
+            StringBuilder: 
+                StringBuilder str.append(a+ b +c).append(" ");
