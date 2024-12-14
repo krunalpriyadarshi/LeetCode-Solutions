0833dcaabfb824a62de688373439f2ec7aafcb4a
Krunal Priyadarshi <krunalpriyadarshi99@gmail.com>
2024-12-14 07:33:08 -0500

    14th dec

--- Test.java
+++ Test.java
@@ -1,34 +1,17 @@
-import java.util.HashSet;
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        long sum = 0;
+        int len = nums.length;
 
-public class Test {
-    public static void main(String[] args) {   
-        int[] nums ={3,-2,1,1};
-        
-        for(int i : constructTransformedArray(nums)){
-            System.out.print(i + " ");
+        for(int i = 0; i < len; i++){
+            int j = i;
+            
+
+            // number of subarray can be created:
+            System.out.println(count);
+            sum += (count/ 2.0) *(count + 1);
         }
 
-        nums = new int[]{-1,4,-1};
-
-        
-        for(int i : constructTransformedArray(nums)){
-            System.out.print(i + " ");
-        }
-    }
-
-    public static int[] constructTransformedArray(int[] nums) {
-        int[] result = new int[nums.length];
-
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] > 0){
-                result[i] = nums[(nums[i] + i) % nums.length];
-            }
-            else if(nums[i] < 0){
-                result[i] = nums[(nums.length + (nums[i] + i)) % nums.length];
-            }
-            else
-                result[i] = 0;
-        }
-        return result;
+        return sum;
     }
 }
