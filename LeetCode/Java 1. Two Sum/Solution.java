0e42545ced795016df53dfffbd44d10374e89d98
Krunal Priyadarshi <KrunalPriyadarshi@carfax.com>
2026-01-24 13:21:51 -0500

    2Sum

---
+++ LeetCode/Java 1. Two Sum/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> hmap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int tap=target-nums[i];
+            if(hmap.containsKey(tap)){
+                 return new int[]{hmap.get(tap),i};
+            }
+            hmap.put(nums[i],i);
+        }
+        return null;
+        
+    }
+}
